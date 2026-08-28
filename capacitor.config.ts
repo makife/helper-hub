52bbca73c103f9a5ac3da3253cf5b848f43fb583
makife <makifergan@gmail.com>
2026-08-28 17:49:44 +0300

    Change appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.p4f1f9c2a8b8f4b3ea935d37f45722ead',
+  appId: 'com.ergan.bielat',
   appName: 'task-hand-shake',
   webDir: 'dist',
   server: {
